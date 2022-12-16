34ff1bfc227d63c866b2285301df05e6bd87a1b5
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2022-12-16 17:23:17 -0300

    Changing name of root directory and removing projects to folders

---
+++ WebApi-SqlLite/WebApi.Sqlite.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebApi.Sqlite.FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ WebApi-SqlLite/WebApi.Sqlite.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebApi.Sqlite.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
