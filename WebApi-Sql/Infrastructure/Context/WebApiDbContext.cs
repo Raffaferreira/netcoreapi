c3c09ef45ef103adbdbb897f296c38dc8921656a
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2023-01-02 02:31:22 -0300

    adding connection string from sql server database created

--- WebApi-Sql/Infrastructure/Context/WebApiDbContext.cs
+++ WebApi-Sql/Infrastructure/Context/WebApiDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Context
 {
@@ -11,6 +12,13 @@ namespace Infrastructure.Context
         public WebApiDbContext(DbContextOptions<WebApiDbContext> options)
         : base(options)
         {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=DESKTOP-VP7F5C3\SQLEXPRESS;Initial Catalog=WebApi;Integrated Security=True")
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
 
         public DbSet<Customer> Customer => Set<Customer>();
         public DbSet<Debito> Debito => Set<Debito>();
